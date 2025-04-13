b17be2bd2adbd8c945326adc4518e6fadce0643a
Anna Bennaim <anna.bennaim@dauphine.eu>
2025-04-13 23:55:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,9 @@ df = pd.read_excel(uploaded_file, engine='openpyxl', skiprows=7)
 
 
 tickers = df.iloc[:, 0].dropna().unique().tolist()
-tickers = [str(t).strip() for t in tickers]
+st.write("Liste des tickers extraits :")
+st.write(tickers)  # Affiche les tickers extraits pour débogage
+tickers = [str(t).strip() for t in tickers if isinstance(t, str)]  # Vérifie que c'est une chaîne de caractères
 
 
 try:
